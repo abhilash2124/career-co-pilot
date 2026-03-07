dedb4d3072ac07794cae608e3de135f46bb4fa77
abhilash2124 <abhilashaddagatla86@gmail.com>
2026-03-07 15:40:19 +0530

    Add gunicorn for production server

--- backend/database.py
+++ backend/database.py
@@ -1,17 +1,8 @@
 import os
-# import sqlite3
 import psycopg2
 from dotenv import load_dotenv
 
 load_dotenv()
-
-# def get_db_connection():
-#     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-#     DB_PATH = os.path.join(BASE_DIR, "career.db")
-    
-#     conn = sqlite3.connect(DB_PATH)
-#     conn.row_factory = sqlite3.Row
-#     return conn
 
 def get_db_connection():
     conn = psycopg2.connect(
